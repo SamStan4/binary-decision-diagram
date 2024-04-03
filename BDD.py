16f3901e8629e7b19e545ddfc3a1c7b40a14eed8
Samuel Stanley <sam.charles.stanley@gmail.com>
2024-04-02 17:10:36 -0700

    idk

--- BDD.py
+++ BDD.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 from pyeda.inter import * # grabs us all of the functions and symbols from the pyeda library
+from pyeda.boolalg.bdd import *
 import os # for clearing the terminal
 
 # this is for changing the color of the terminal, these are the same escape sequences
@@ -26,6 +27,9 @@ class colors:
     B_WHITE = '\033[47m'
     
     RESET = '\033[0m'
+
+x_variables = [exprvar('x{}'.format(i)) for i in range(1, 7)]     # this will create the boolean variables that we are going to want 
+y_variables = [exprvar('y{}'.format(i)) for i in range(1, 7)]     # and format it in the form { x1, x2, ... , x6 } and { y1, y2, ... , y6 }
 
 #*********************************************************************************************************************************************#
 #   function name       : convert_int_to_binary_6_bit_array()
@@ -55,6 +59,57 @@ def convert_int_to_binary_6_bit_array(target):
     return boolean_list
 
 #*********************************************************************************************************************************************#
+#   function name       : make_edge_bool_formula()
+#   functional purpose  : this function will create a boolean formula that represnets that there is an edge between two nodes in a graph
+#   inputs              : two integer variables
+#   outputs             : boolean formula
+#   preconditions       : none
+#   postconditions      : none
+#   programmer          : Sam Stanley
+
+def make_edge_bool_formula(x_node, y_node):
+    #x1, x1, x3 = map()
+
+    forumla_function = None
+
+    x_node_list = convert_int_to_binary_6_bit_array(x_node)     # convert both of these into boolean arrays length 6
+    y_node_list = convert_int_to_binary_6_bit_array(y_node)
+
+    if (x_node_list[0]):           # initialize to be the first x variable
+        forumla_function = x_variables[0]
+    else:
+        forumla_function = ~x_variables[0]
+
+    for i in range (1, 6, 1):      # add the x variables
+        if (x_node_list[i]):
+            forumla_function &= x_variables[i]
+        else:
+            forumla_function &= ~x_variables[i]
+
+    for i in range (0, 6, 1):     # add the y variables
+        if (y_node_list[i]):
+            forumla_function &= y_variables[i]
+        else:
+            forumla_function &= ~y_variables[i]
+
+    return expr2bdd(forumla_function)
+
+#*********************************************************************************************************************************************#
+#   function name       : make_32_node_BDD()
+#   functional purpose  : this function answers the question: (i + 3)%32 = j%32 or (i + 8)%32 = j%32.
+#   inputs              : int, int
+#   outputs             : boolean
+#   preconditions       : none
+#   postconditions      : none
+#   programmer          : Sam Stanley
+
+def edge_rule(x_node, y_node):
+    if ((((x_node + 3) % 32) == (y_node % 32)) or (((x_node + 8) % 32) == (y_node % 32))):
+        return True
+    else:
+        return False
+
+#*********************************************************************************************************************************************#
 #   function name       : make_32_node_BDD()
 #   functional purpose  : this function will create a 32 (0 - 32) node BDD representation of a graph with edge rule: 
 #                         there is an edge from node i to node j iff (i + 3)%32 = j%32 or (i + 8)%32 = j%32.
@@ -64,20 +119,23 @@ def convert_int_to_binary_6_bit_array(target):
 #   postconditions      : none
 #   programmer          : Sam Stanley
 
-
-def make_edge(node_one, node_two):
-    return "hi"
-
 def make_32_node_BDD():
 
-    #binary_32 = convert_int_to_binary_6_bit_array(64)
+    temp_boolean_formula = None
+    local_boolean_formula = None
 
     for i in range(0, 32, 1):
         for j in range(0, 32, 1):
-            if ((((i + 3) % 32) == (j % 32)) or (((i + 8) % 32) == (j % 32))):
-                print("there is an edge from node " + str(i) + " --> node " + str(j))
+            if (edge_rule(i, j)):
+                temp_boolean_formula = make_edge_bool_formula(i, j)
+                if (local_boolean_formula == None):
+                    local_boolean_formula = temp_boolean_formula
+                else:
+                    local_boolean_formula |= temp_boolean_formula
 
-    return "hi"
+    return expr2bdd(local_boolean_formula)
+
+
 
 #*********************************************************************************************************************************************#
 #   function name       : clear_terminal()
@@ -138,7 +196,25 @@ def main():
     if (decision == 1):
         test_BDD = make_32_node_BDD()
 
-    print(test_BDD)
+    #print(bdd2expr(test_BDD))
+
+    smoothing_vars = test_BDD.smoothing()
+
+    #print(bdd2expr(smoothing_vars))
+
+    print("Smoothing Variables:", smoothing_vars)
+
+
+    #esult = test_BDD.
+
+    #my_formula = x_variables[0] & y_variables[0]
+
+    #print(my_formula)
+        
+    #bool_for = make_edge_bool_formula(11, 6)
+
+    #print(bool_for)
+
 
 
 
